1ceb328eb18f59c3aeb67d258527cb662418c100
SHAWank <shawank17198@gmail.com>
2017-07-27 10:37:23 +0000

    template is in bootstrap.html

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 		<link href="safekart.css" type="text/css" rel="stylesheet" />
 		<link rel="stylesheet" href="bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-  		<script src ="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></script>
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   		<link href="https://fonts.googleapis.com/css?family=Lato|Open+Sans|Slabo+27px" rel="stylesheet">
 
 	</head>
